0a15b0f3b8462ebd3956c916ed860ae4dd4d7daa
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2025-02-25 18:30:50 +0000

    add pybind

---
+++ flashinfer/jit/aiter_decode_templ.py
@@ -0,0 +1,36 @@
+# TODO: license header
+aiter_decode_templ = {
+    '.hip': r"""// generated with aiter_decode_templ.py
+
+""",
+    '_pybind.cc': r"""// generated with aiter_decode_templ.py
+#include "pytorch_extension_utils.h"
+
+void paged_attention(
+    torch::Tensor& out, // [num_seqs, num_heads, head_size]
+    torch::Tensor& workspace_buffer,
+    torch::Tensor& query,       // [num_seqs, num_heads, head_size]
+    torch::Tensor& key_cache,   // [num_blocks, num_heads, block_size, head_size] or
+                                // [num_blocks, block_size, num_heads, head_size]
+    torch::Tensor& value_cache, // [num_blocks, num_heads, block_size, head_size] or
+                                // [num_blocks, block_size, num_heads, head_size]
+    double scale,
+    torch::Tensor& kv_indptr,         // [num_seqs + 1]
+    torch::Tensor& kv_page_indices,   // [max_num_blocks]
+    std::optional<torch::Tensor>& kv_last_page_lens, // [num_seqs]
+    int64_t block_size,
+    int64_t max_num_partitions,
+    const std::optional<torch::Tensor>& alibi_slopes,
+    const std::string& kv_cache_dtype,
+    const std::string& kv_cache_layout,
+    float logits_soft_cap,
+    torch::Tensor& k_scale,
+    torch::Tensor& v_scale,
+    const c10::optional<torch::Tensor>& fp8_out_scale,
+    int64_t partition_size);
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
+  m.def("run", &paged_attention,
+        "AITER paged attention");
+""",
+}
