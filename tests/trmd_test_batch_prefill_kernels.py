cdb3e99e5ce2649d9e6c5e917403ba4170966c1e
Po Yen, Chen <PoYen.Chen@amd.com>
2025-02-27 18:47:05 +0000

    Add causal=True test cases

--- tests/trmd_test_batch_prefill_kernels.py
+++ tests/trmd_test_batch_prefill_kernels.py
@@ -21,6 +21,8 @@ from jit_utils import jit_prefill_attention_func_args
 import flashinfer
 import math
 
+from einops import rearrange, repeat
+
 
 @pytest.fixture(autouse=True, scope="module")
 def warmup_jit():
@@ -45,17 +47,69 @@ def warmup_jit():
         finally:
             yield
 
+def construct_local_mask(
+    seqlen_q,
+    seqlen_k,
+    window_size=(-1, -1),  # -1 means infinite window size
+    query_padding_mask=None,
+    key_padding_mask=None,
+    device=None,
+    key_leftpad=None,
+):
+    row_idx = rearrange(torch.arange(seqlen_q, device=device, dtype=torch.long), "s -> s 1")
+    col_idx = torch.arange(seqlen_k, device=device, dtype=torch.long)
+    if key_leftpad is not None:
+        key_leftpad = rearrange(key_leftpad, "b -> b 1 1 1")
+        col_idx = repeat(col_idx, "s -> b 1 1 s", b=key_leftpad.shape[0])
+        col_idx = torch.where(col_idx >= key_leftpad, col_idx - key_leftpad, 2**32)
+    sk = (
+        seqlen_k
+        if key_padding_mask is None
+        else rearrange(key_padding_mask.sum(-1), "b -> b 1 1 1")
+    )
+    sq = (
+        seqlen_q
+        if query_padding_mask is None
+        else rearrange(query_padding_mask.sum(-1), "b -> b 1 1 1")
+    )
+    if window_size[0] < 0:
+        return col_idx > row_idx + sk - sq + window_size[1]
+    else:
+        sk = torch.full_like(col_idx, seqlen_k) if key_padding_mask is None else sk
+        return torch.logical_or(
+            col_idx > torch.minimum(row_idx + sk - sq + window_size[1], sk),
+            col_idx < row_idx + sk - sq - window_size[0],
+        )
+
 def ref_masked_attention(
     query: torch.Tensor,
     key: torch.Tensor,
     value: torch.Tensor,
+    causal: bool = False,
+    window_left: int = -1,
     logits_soft_cap: float = 0.0
 ) -> torch.Tensor:
+    if causal:
+        window_size = (window_left, 0)
+    else:
+        window_size = (-1, -1)
+
     head_dim = query.shape[2]
+    seqlen_q = query.shape[0]
+    seqlen_k = key.shape[0]
     scale = 1.0 / math.sqrt(head_dim)
+
     attn_weights = scale * torch.einsum("qhd,khd->hqk", query.float(), key.float())
     if 0 < logits_soft_cap:
         attn_weights = logits_soft_cap * torch.tanh(attn_weights / logits_soft_cap)
+    if window_size[0] >= 0 or window_size[1] >= 0:
+        local_mask = construct_local_mask(
+            seqlen_q,
+            seqlen_k,
+            window_size,
+            device=query.device,
+        )
+        attn_weights.masked_fill_(local_mask, float("-inf"))
     attn_weights = torch.softmax(attn_weights, dim=-1)
     out = torch.einsum("hqk,khd->qhd", attn_weights, value.float())
     return out.to(query)
@@ -66,7 +120,7 @@ def ref_masked_attention(
 @pytest.mark.parametrize("page_size", [1, 16])
 @pytest.mark.parametrize("num_qo_kv_heads", [(2, 1), (4, 1)])
 @pytest.mark.parametrize("head_dim", [64, 128])
-@pytest.mark.parametrize("causal", [False])
+@pytest.mark.parametrize("causal", [False, True])
 @pytest.mark.parametrize("kv_layout", ["HND", "NHD"])
 @pytest.mark.parametrize("pos_encoding_mode", ["NONE"])
 @pytest.mark.parametrize("use_cuda_graph", [False])
@@ -276,15 +330,14 @@ def test_batch_prefill_with_paged_kv_cache(
             logits_soft_cap=logits_soft_cap,
         )
         """
-        # NOTICE: for now, we use alibi_attention() as reference function
-        assert not causal
+        # NOTICE: for now, we use ref_masked_attention() as reference function
         assert pos_encoding_mode == 'NONE'
         assert not return_lse
 
         rtol = 1e-3 if dtype == torch.float16 else 1e-2
         atol = 1e-3 if dtype == torch.float16 else 1e-2 
 
-        o_ref_i = ref_masked_attention(qi, ki, vi, logits_soft_cap)
+        o_ref_i = ref_masked_attention(qi, ki, vi, causal=causal, logits_soft_cap=logits_soft_cap)
         o_i = o[q_indptr_cpu[i] : q_indptr_cpu[i + 1]]
         torch.testing.assert_close(o_i, o_ref_i, rtol=rtol, atol=atol)
 
