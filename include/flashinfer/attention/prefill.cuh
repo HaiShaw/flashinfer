b687e9d88a2fe98eaff30a6d79543f4b6b86f169
Hashem Hashemi <hashem.hashemi@amd.com>
2025-03-26 08:52:21 +0000

    Change bfloat conversion to fast-unsafe.

--- include/flashinfer/attention/prefill.cuh
+++ include/flashinfer/attention/prefill.cuh
@@ -1006,7 +1006,15 @@ __device__ __forceinline__ void compute_sfm_v(AttentionVariant variant,
       for (uint32_t mma_kv = 0; mma_kv < NUM_MMA_KV; ++mma_kv) {
         //vec_cast<DTypeQ, float>::cast<8>(s_frag_f16[mma_q][mma_kv], s_frag[mma_q][mma_kv]);
 //hipFIXED
-        vec_cast<DTypeQ, float>::template cast<4>(s_frag_f16[mma_q][mma_kv], s_frag[mma_q][mma_kv]);
+        if constexpr (std::is_same_v<DTypeQ, gpu_bfloat16>) {
+	  //fast but unsafe bfloat conversion... 	
+          for (size_t i = 0; i < 4; ++i)
+	    s_frag_f16[mma_q][mma_kv][i] = 
+		    *reinterpret_cast<DTypeQ*>(&(reinterpret_cast<uint16_t*>(&s_frag[mma_q][mma_kv][i])[1]));  
+	}
+	else {
+          vec_cast<DTypeQ, float>::template cast<4>(s_frag_f16[mma_q][mma_kv], s_frag[mma_q][mma_kv]);
+	}
       }
     }
   }
